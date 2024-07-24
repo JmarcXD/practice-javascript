1138c54c516ad67a1df9221246da1522466a0f34
Jmarc13 <jmarcparagas.doc@gmail.com>
2024-07-24 13:40:15 +0200

    nice

--- exercise10/index.js
+++ exercise10/index.js
@@ -19,6 +19,7 @@ function verifyLetter() {
         }
     }
 
+    drawHangman(numberHP);
 
     if (numberHP == 0) // If he has 0 hp, loose the game
         document.getElementById("message").innerHTML = "HAS PERDIDO LA PALABRA ERA " + wordToGuess;
@@ -44,7 +45,7 @@ function findLetterTest(letter) {
     return letterFound;
 }
 
-function displayLetterUsed(letter) { 
+function displayLetterUsed(letter) {
     letterUsedList.push(letter); // Add in the array
     let displayLetterUsed = document.getElementById("letterUsed");
 
@@ -53,3 +54,72 @@ function displayLetterUsed(letter) {
         displayLetterUsed.innerHTML += " ," + lu;
     });
 }
+
+const canvas = document.getElementById('hangman');
+const ctx = canvas.getContext('2d');
+
+function drawGallows() {
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(100, 350);
+    ctx.lineTo(300, 350);
+    ctx.moveTo(150, 350);
+    ctx.lineTo(150, 50);
+    ctx.lineTo(250, 50);
+    ctx.lineTo(250, 100);
+    ctx.stroke();
+}
+
+function drawHead() {
+    ctx.beginPath();
+    ctx.arc(250, 130, 30, 0, Math.PI * 2);
+    ctx.stroke();
+}
+
+function drawBody() {
+    ctx.beginPath();
+    ctx.moveTo(250, 160);
+    ctx.lineTo(250, 250);
+    ctx.stroke();
+}
+
+function drawLeftArm() {
+    ctx.beginPath();
+    ctx.moveTo(250, 180);
+    ctx.lineTo(210, 220);
+    ctx.stroke();
+}
+
+function drawRightArm() {
+    ctx.beginPath();
+    ctx.moveTo(250, 180);
+    ctx.lineTo(290, 220);
+    ctx.stroke();
+}
+
+function drawLeftLeg() {
+    ctx.beginPath();
+    ctx.moveTo(250, 250);
+    ctx.lineTo(210, 300);
+    ctx.stroke();
+}
+
+function drawRightLeg() {
+    ctx.beginPath();
+    ctx.moveTo(250, 250);
+    ctx.lineTo(290, 300);
+    ctx.stroke();
+}
+
+function drawHangman(lives) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(lives);
+    if (lives < 7) drawGallows();
+    if (lives < 6) drawHead();
+    if (lives < 5) drawBody();
+    if (lives < 4) drawLeftArm();
+    if (lives < 3) drawRightArm();
+    if (lives < 2) drawLeftLeg();
+    if (lives < 1) drawRightLeg();
+}
+
